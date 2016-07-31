deb810fcc73b146dae819642979844242e078963
Luis Rascao <luis.rascao@miniclip.com>
2016-07-31 22:18:30 +0100

    Add two new fields to relapp_srv2 worker process state

--- apps/relapp/src/relapp_srv2.erl
+++ apps/relapp/src/relapp_srv2.erl
@@ -8,7 +8,9 @@
 -behaviour(gen_server).
 
 -record(state, {
-    id :: non_neg_integer()
+    id :: non_neg_integer(),
+    name = <<"">> :: binary(),
+    description = <<"">> :: binary()
 }).
 %% declare the record that holds the state for this gen_server
 -state_record(state).
@@ -30,7 +32,9 @@ set_state(State) ->
     ok = gen_server:call(?MODULE, {set_state, State}).
 
 init([]) ->
-    {ok, #state{id = 0}}.
+    {ok, #state{id = 0,
+                 name = <<"name">>,
+                 description = <<"description">>}}.
 
 handle_call({set_state, NewState}, _From, _State) ->
     {reply, ok, NewState};
@@ -46,5 +50,9 @@ handle_info(_Info, State) ->
 terminate(_Reason, _State) ->
     ok.
 
-code_change(_OldVsn, State, _Extra) ->
+code_change(OldVsn, State, _Extra) ->
+    Attributes = relapp_srv2:module_info(attributes),
+    Vsn = proplists:get_value(vsn, Attributes),
+    io:format("code change from ~p(~p) to ~p(~p)",
+        [OldVsn, State, Vsn, State]),
     {ok, State}.
