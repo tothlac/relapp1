563c4ac642bf912949ca00db5553da57a7303c5c
Luis Rascao <luis.rascao@miniclip.com>
2016-08-12 17:41:17 +0100

    Store function in a gen_server's state sent from a module

--- apps/relapp/src/relapp_m1.erl
+++ apps/relapp/src/relapp_m1.erl
@@ -1,5 +1,12 @@
 -module(relapp_m1).
 
--export([test/1]).
+-export([test/1,
+         store_fun/0]).
 
 test(Arg) -> {ok, Arg}.
+
+store_fun() ->
+    F = fun() ->
+            io:format("hello, i'm a function\n")
+        end,
+    relapp_srv2:store_fun(F).
--- apps/relapp/src/relapp_srv2.erl
+++ apps/relapp/src/relapp_srv2.erl
@@ -10,13 +10,16 @@
 -record(state, {
     id :: non_neg_integer(),
     name = <<"">> :: binary(),
-    description = <<"">> :: binary()
+    description = <<"">> :: binary(),
+    f = undefined :: undefined | function()
 }).
 %% declare the record that holds the state for this gen_server
 -state_record(state).
 
 -export([start_link/0,
-         set_state/1]).
+         set_state/1,
+         store_fun/1,
+         call_stored_fun/0]).
 
 -export([init/1,
          handle_call/3,
@@ -31,6 +34,12 @@ start_link() ->
 set_state(State) ->
     ok = gen_server:call(?MODULE, {set_state, State}).
 
+store_fun(Fun) ->
+    gen_server:cast(?MODULE, {store_fun, Fun}).
+
+call_stored_fun() ->
+    gen_server:cast(?MODULE, call_stored_fun).
+
 init([]) ->
     {ok, #state{id = 0,
                  name = <<"name">>,
@@ -41,6 +50,12 @@ handle_call({set_state, NewState}, _From, _State) ->
 handle_call(_Event, _From, State) ->
     {reply, ok, State}.
 
+handle_cast({store_fun, Fun}, State) ->
+    {noreply, State#state{f = Fun}};
+handle_cast(call_stored_fun,
+            #state{f = Fun} = State) ->
+    Fun(),
+    {noreply, State};
 handle_cast(_Event, State) ->
     {noreply, State}.
 
