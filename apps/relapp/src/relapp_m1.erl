67836bfa85dcfc1d10aa629c945b0a4702cbcd27
Luis Rascao <luis.rascao@miniclip.com>
2016-08-12 17:43:13 +0100

    Simple change in function that is sent to the gen_server's state

--- apps/relapp/src/relapp_m1.erl
+++ apps/relapp/src/relapp_m1.erl
@@ -7,6 +7,6 @@ test(Arg) -> {ok, Arg}.
 
 store_fun() ->
     F = fun() ->
-            io:format("hello, i'm a function\n")
+            io:format("hello friend, i'm a function\n")
         end,
     relapp_srv2:store_fun(F).
