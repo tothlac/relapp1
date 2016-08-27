1eb7ccd63db34841a350d2bc165bb627785970b4
Luis Rascao <luis.rascao@miniclip.com>
2016-08-27 14:59:57 +0100

    Add set_description_id/1, get_description_id/0 methods
    Sets/gets an id in the relapp_srv gen_server

--- apps/relapp/src/relapp_srv.erl
+++ apps/relapp/src/relapp_srv.erl
@@ -10,11 +10,15 @@
 -record(state, {
     id  :: non_neg_integer(),
     description_id :: non_neg_integer(),
-    name = undefined :: undefined | binary()
+    name = undefined :: undefined | binary(),
+    extra = undefined :: undefined | binary()
 }).
 
 -export([start_link/0,
-         test/1]).
+         test/1,
+         set_description_id/1,
+         get_description_id/0,
+         get_extra/0]).
 
 -export([init/1,
          handle_call/3,
@@ -30,6 +34,15 @@ test(undefined) -> ok;
 test(Arg) ->
     gen_server:call(?MODULE, {test, Arg}).
 
+set_description_id(Arg) ->
+    gen_server:call(?MODULE, {set_description_id, Arg}).
+
+get_description_id() ->
+    gen_server:call(?MODULE, get_description_id).
+
+get_extra() ->
+    gen_server:call(?MODULE, get_extra).
+
 init([]) ->
     {ok, #state{id = 0,
                 description_id = 1,
@@ -38,6 +51,14 @@ init([]) ->
 handle_call({test, Arg}, _From, State) ->
     Ret = relapp_m1:test(Arg),
     {reply, Ret, State};
+handle_call({set_description_id, Arg}, _From, State) ->
+    {reply, ok,
+     State#state{description_id = Arg}};
+handle_call(get_description_id, _From,
+            #state{description_id = DescriptionId} = State) ->
+    {reply, {ok, DescriptionId}, State};
+handle_call(get_extra, _From, State) ->
+    {reply, State#state.extra, State};
 handle_call(_Event, _From, State) ->
     {reply, ok, State}.
 
