ffc01fbb9063acf99d32287c2d90ad37ee6b3d5b
Luis Rascao <luis.rascao@gmail.com>
2017-08-14 22:04:57 +0100

    Add new module that implements two behaviours
    Both application and supervisor behaviour.

--- apps/relapp/src/relapp_app_sup.erl
+++ apps/relapp/src/relapp_app_sup.erl
@@ -1,18 +1,21 @@
 %%%-------------------------------------------------------------------
-%% @doc relupapp top level supervisor.
+%% @doc
 %% @end
 %%%-------------------------------------------------------------------
 
--module(relapp_sup).
+-module(relapp_app_sup).
 
 -behaviour(supervisor).
+-behaviour(application).
 
 %% API
--export([start_link/0,
-         helper_method/0]).
+-export([start_link/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
+%% Application callbacks
+-export([start/2,
+         stop/1]).
 
 -define(SERVER, ?MODULE).
 
@@ -23,7 +26,15 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-helper_method() -> ok.
+%%====================================================================
+%% Application callbacks
+%%====================================================================
+
+start(_StartType, _StartArgs) ->
+    relapp_sup:start_link().
+
+stop(_State) ->
+    ok.
 
 %%====================================================================
 %% Supervisor callbacks
@@ -32,13 +43,7 @@ helper_method() -> ok.
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
     {ok, { {one_for_all, 0, 1},
-            [{relapp_srv, {relapp_srv, start_link, []},
-              transient, 5000, worker, [relapp_srv]},
-             {relapp_srv2, {relapp_srv2, start_link, []},
-              transient, 5000, worker, [relapp_srv2]},
-             {relapp_app_sup, {relapp_app_sup, start_link, []},
-              transient, 5000, supervisor, [relapp_app_sup]}
-         ]}}.
+            []}}.
 
 %%====================================================================
 %% Internal functions
